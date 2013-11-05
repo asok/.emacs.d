3a0770d8e7fd7e882b72c979f232404b432714db
asok <adam.sokolnicki@gmail.com>
2013-11-05 20:23:41 +0100

    Insert \n after expanding snippet.

--- inits/init-yasnippet.el
+++ inits/init-yasnippet.el
@@ -2,3 +2,16 @@
 (require 'yasnippet)
 (setq yas/root-directory "~/.emacs.d/snippets")
 (yas/load-directory yas/root-directory)
+
+(defun asok/insert-new-line-maybe ()
+  (save-excursion
+    (goto-char yas-snippet-end)
+    (unless (or (looking-at "\n\n") (looking-at "\n[\t ]+end"))
+      (insert "\n"))))
+
+(add-hook 'enh-ruby-mode-hook 'asok/register-ruby-yas-after-exit-snippet-hook)
+(defun asok/register-ruby-yas-after-exit-snippet-hook ()
+  (add-hook 'yas-after-exit-snippet-hook 'asok/insert-new-line-maybe t t))
+
+
+
