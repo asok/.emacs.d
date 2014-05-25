652d71d3dee8b119b3c5b63511c56d31c5b07b31
asok <adam.sokolnicki@gmail.com>
2014-05-25 13:07:52 +0200

    Correct js2-mode init file.

--- inits/init-js2-mode.el
+++ inits/init-js2-mode.el
@@ -1,5 +1,5 @@
-(add-to-list 'auto-mode-alist '("\\.js\\$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 (setq js2-strict-missing-semi-warning nil)
 (setq-default js2-basic-offset 2)
 (eval-after-load "js2-mode"
