b99d71efcce6586e9ddecb55449cc3bb87f42a44
asok <adam.sokolnicki@gmail.com>
2014-11-15 14:14:29 +0100

    Add themes directory.

--- theming.el
+++ theming.el
@@ -1,6 +1,13 @@
 (defadvice load-theme
-  (before theme-dont-propagate activate)
+    (before theme-dont-propagate activate)
   (mapcar #'disable-theme custom-enabled-themes))
+
+(defun asok/discover-themes ()
+  (let ((root "~/.emacs.d/themes/"))
+    (mapcar #'(lambda (dir) (add-to-list 'custom-theme-load-path (concat root dir)))
+            (directory-files root nil "^[^.]+$"))))
+
+(asok/discover-themes)
 
 (require 'solarized)
 
