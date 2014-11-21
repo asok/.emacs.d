780b4245cc13672588b1e05bde4cd98fa2e080ba
asok <adam.sokolnicki@gmail.com>
2014-11-21 17:13:49 +0100

    Make smart keybindings for expand region in evil's visual state.

--- inits/init-expand-region.el
+++ inits/init-expand-region.el
@@ -1,4 +1,4 @@
-(global-set-key (kbd "s-,") 'er/expand-region)
-(global-set-key (kbd "C-s-,") 'er/contract-region)
+(evil-define-key 'visual global-map (kbd "v") 'er/expand-region)
+(evil-define-key 'visual global-map (kbd "C-v") 'er/contract-region)
 
 (provide 'inits/expand-region)
