5e794413ee922d35b5da5b6d16674aa978a2e68e
asok <adam.sokolnicki@gmail.com>
2014-11-21 17:13:24 +0100

    Tryout the aggressive-indent for clojure and css files.

--- inits/init-aggressive-indent.el
+++ inits/init-aggressive-indent.el
@@ -1,3 +1,3 @@
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
-(add-hook 'css-mode #'aggressive-indent-mode)
-(add-hook 'clojure-mode #'aggressive-indent-mode)
+(add-hook 'css-mode-hook #'aggressive-indent-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
