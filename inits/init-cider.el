b9eb6a83ea8d987103d9ef1ceb27f38ab132c875
asok <adam.sokolnicki@gmail.com>
2014-11-27 23:08:02 +0100

    Add asok/cider-repl-kill-input command

--- inits/init-cider.el
+++ inits/init-cider.el
@@ -3,7 +3,14 @@
       clojure-use-backtracking-indent nil)
 
 (evil-define-key 'normal cider-mode-map (kbd "RET") 'cider-jump-to-var)
+
 (require 'clojure-mode)
 (define-key clojure-mode-map (kbd "RET") 'newline-and-indent)
 
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+(defun asok/cider-repl-clear-input ()
+  (interactive)
+  (cider-repl-delete-current-input)
+  (call-interactively 'evil-append-line))
+(evil-define-key 'normal cider-repl-mode-map (kbd "S") 'asok/cider-repl-clear-input)
