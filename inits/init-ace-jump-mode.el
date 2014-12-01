66eb8f56c8cd3e8488e71b8288ffaa1b7ebcbb2e
asok <adam.sokolnicki@gmail.com>
2014-12-01 15:42:53 +0100

    Change bindings for ace-jumo-mode

--- inits/init-ace-jump-mode.el
+++ inits/init-ace-jump-mode.el
@@ -1,5 +1,5 @@
 (asok/evil-define-key-when-global-map '(normal motion)
-                                      (asok/leader-kbd "g") 'evil-ace-jump-word-mode
-                                      (asok/leader-kbd "G") 'evil-ace-jump-char-mode)
+                                      (asok/leader-kbd "SPC") 'evil-ace-jump-word-mode
+                                      (asok/leader-kbd "C-SPC") 'evil-ace-jump-char-mode)
 
 (setq ace-jump-mode-scope 'window)
