690c85aefc94db3b44c94542c1f9cdd7e0b30056
asok <adam.sokolnicki@gmail.com>
2015-01-19 13:47:36 +0100

    Add more services to prodigy

--- inits/init-prodigy.el
+++ inits/init-prodigy.el
@@ -6,4 +6,27 @@
   :tags '(macbook)
   :kill-signal 'sigkill)
 
+(prodigy-define-service
+  :name "navigart-api"
+  :command "shotgun"
+  :cwd "~/projects/navigart-api"
+  :tags '(macbook)
+  :kill-signal 'sigkill)
+
+(prodigy-define-service
+  :name "navigart3-v2"
+  :command "grunt"
+  :args '("server")
+  :cwd "~/projects/navigart3-v2"
+  :tags '(macbook)
+  :kill-signal 'sigkill)
+
+(prodigy-define-service
+  :name "Redis"
+  :command "redis-server"
+  :args '("/usr/local/etc/redis.conf")
+  :cwd "~"
+  :tags '(macbook)
+  :kill-signal 'sigkill)
+
 (evil-add-hjkl-bindings prodigy-mode-map 'emacs)
