a634584f48f05fb12db7147a3cbee959dcec4938
asok <adam.sokolnicki@gmail.com>
2015-01-19 13:47:14 +0100

    Remove advice on compilation-mode start

--- inits/init-projectile.el
+++ inits/init-projectile.el
@@ -5,15 +5,15 @@
 (evil-define-key 'normal projectile-mode-map (kbd ", p") 'evil-projectile-mode-map)
 
 
-(defadvice compilation-start (before projectile-set-current-projectile-root activate)
-  (when (projectile-project-p)
-    (setq projectile-current-root (projectile-project-root))))
+;; (defadvice compilation-start (before projectile-set-current-projectile-root activate)
+;;   (when (projectile-project-p)
+;;     (setq projectile-current-root (projectile-project-root))))
 
 (defun asok/set-compilation-save-buffer-predicate ()
   (setq-local compilation-save-buffers-predicate '(lambda ()
-						    (string-prefix-p
-						     projectile-current-root
-						     (file-truename (buffer-file-name))))))
+                                                    (string-prefix-p
+                                                     projectile-current-root
+                                                     (file-truename (buffer-file-name))))))
 
 (add-hook 'projectile-mode-hook 'asok/set-compilation-save-buffer-predicate nil t)
 
