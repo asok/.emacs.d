32473432affce3268317456759b6e25797530830
asok <adam.sokolnicki@gmail.com>
2015-01-23 16:52:56 +0100

    Leave chruby to be hooked only in ruby-mode

--- inits/init-chruby.el
+++ inits/init-chruby.el
@@ -1,10 +1,3 @@
 (require 'chruby)
 
 (add-hook 'ruby-mode-hook #'chruby-use-corresponding)
-
-(defun asok/chruby-use-corresponding ()
-  ;; it will be set for ruby-mode already
-  (when (not (eq major-mode 'ruby-mode))
-    (chruby-use-corresponding)))
-
-'(add-hook 'projectile-switch-project #'asok/chruby-use-corresponding))
