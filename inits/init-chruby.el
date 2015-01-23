5c2cd3c376b3c48320163ed96b697ea8702ee00c
asok <adam.sokolnicki@gmail.com>
2015-01-23 13:05:05 +0100

    Run chruby-activate-corresponding on switching projects

--- inits/init-chruby.el
+++ inits/init-chruby.el
@@ -1,2 +1,10 @@
 (require 'chruby)
+
 (add-hook 'ruby-mode-hook #'chruby-use-corresponding)
+
+(defun asok/chruby-use-corresponding ()
+  ;; it will be set for ruby-mode already
+  (when (not (eq major-mode 'ruby-mode))
+    (chruby-use-corresponding)))
+
+'(add-hook 'projectile-switch-project #'asok/chruby-use-corresponding))
