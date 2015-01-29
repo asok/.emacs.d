b1ab134cfdb290413aedf4409be0917a84f798fc
asok <adam.sokolnicki@gmail.com>
2015-01-29 14:49:36 +0100

    Some cleanup in the magit init file

--- inits/init-magit.el
+++ inits/init-magit.el
@@ -2,9 +2,7 @@
 
 (evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
   (kbd "C-f") 'evil-scroll-page-down
-  (kbd "C-b") 'evil-scroll-page-up
-  (asok/leader-kbd "1") 'delete-other-windows
-  (asok/leader-kbd "0") 'delete-window)
+  (kbd "C-b") 'evil-scroll-page-up)
 
 (evil-add-hjkl-bindings magit-status-mode-map 'emacs
   (kbd "C-f") 'evil-scroll-page-down
@@ -13,22 +11,19 @@
   (kbd "h") 'magit-toggle-diff-refine-hunk
   (kbd "l") 'magit-key-mode-popup-logging
   (kbd "{") 'magit-goto-previous-sibling-section
-  (kbd "}") 'magit-goto-next-sibling-section
-  (asok/leader-kbd "1") 'delete-other-windows
-  (asok/leader-kbd "0") 'delete-window)
+  (kbd "}") 'magit-goto-next-sibling-section)
 
 (evil-add-hjkl-bindings magit-commit-mode-map 'emacs
   (kbd "{") 'magit-goto-previous-section
   (kbd "}") 'magit-goto-next-section
-  (asok/leader-kbd "1") 'delete-other-windows
-  (asok/leader-kbd "0") 'delete-window
   )
 
 (evil-add-hjkl-bindings magit-log-mode-map 'emacs
   (kbd "C-f") 'evil-scroll-page-down
   (kbd "C-b") 'evil-scroll-page-up
-  (asok/leader-kbd "1") 'delete-other-windows
-  (asok/leader-kbd "0") 'delete-window
   )
+
+(evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+  (kbd "K") 'magit-discard-item)
 
 (require 'magit-blame)
