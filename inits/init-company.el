9312b129c55d2649e2bbf315ffe0f67b62bcc96e
asok <adam.sokolnicki@gmail.com>
2015-04-22 17:06:15 +0200

    Ignore case in company complete

--- inits/init-company.el
+++ inits/init-company.el
@@ -2,7 +2,9 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq company-idle-delay nil
-      company-auto-complete nil)
+      company-auto-complete nil
+      company-dabbrev-ignore-case nil
+      company-dabbrev-code-ignore-case nil)
 
 (define-key evil-insert-state-map "\C-n" nil)
 (evil-define-key 'insert company-mode-map (kbd "C-n") 'company-complete)
