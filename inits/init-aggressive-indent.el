edb5192653d78fe0798d34a5af10b5f340b595e2
asok <adam.sokolnicki@gmail.com>
2015-05-03 03:42:50 +0200

    Use aggressive-indent-mode with js2-mode

--- inits/init-aggressive-indent.el
+++ inits/init-aggressive-indent.el
@@ -1,3 +1,4 @@
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 (add-hook 'css-mode-hook #'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+(add-hook 'js2-mode #'aggressive-indent-mode)
