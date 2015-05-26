8993d718f45c3eb24ea85c83572dc5d5f3b1489e
asok <adam.sokolnicki@gmail.com>
2015-05-26 17:41:18 +0200

    Add the user docset for ReactJS

--- inits/init-helm-dash.el
+++ inits/init-helm-dash.el
@@ -3,8 +3,8 @@
 (add-hook 'emacs-lisp-mode-hook '(lambda () (setq-local helm-dash-docsets '("Emacs Lisp"))))
 (add-hook 'ruby-mode-hook '(lambda () (setq-local helm-dash-docsets '("Redis" "Ruby on Rails" "Ruby"))))
 (add-hook 'clojure-mode-hook '(lambda () (setq-local helm-dash-docsets '("Clojure"))))
-(add-hook 'js-mode-hook '(lambda () (setq-local helm-dash-docsets '("UnderscoreJS"))))
-(add-hook 'web-mode-hook '(lambda () (setq-local helm-dash-docsets '("UnderscoreJS"))))
+(add-hook 'js-mode-hook '(lambda () (setq-local helm-dash-docsets '("UnderscoreJS" "React"))))
+(add-hook 'web-mode-hook '(lambda () (setq-local helm-dash-docsets '("UnderscoreJS" "React"))))
 
 (add-hook 'projectile-rails-mode-hook '(lambda()
 																				 (setq-local helm-dash-docsets '("Redis" "Ruby on Rails" "Ruby" "jQuery"))))
