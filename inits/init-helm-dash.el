368c0d854c08acd46ad56818e4ce1886022e8ea8
asok <adam.sokolnicki@gmail.com>
2015-07-18 10:00:47 +0200

    Workouround the helm-dash bug

--- inits/init-helm-dash.el
+++ inits/init-helm-dash.el
@@ -1,10 +1,28 @@
 (require 'helm-dash)
 
-(add-hook 'emacs-lisp-mode-hook '(lambda () (setq-local helm-dash-docsets '("Emacs Lisp"))))
-(add-hook 'ruby-mode-hook '(lambda () (setq-local helm-dash-docsets '("Redis" "Ruby on Rails" "Ruby"))))
-(add-hook 'clojure-mode-hook '(lambda () (setq-local helm-dash-docsets '("Clojure"))))
-(add-hook 'js-mode-hook '(lambda () (setq-local helm-dash-docsets '("UnderscoreJS" "React"))))
-(add-hook 'web-mode-hook '(lambda () (setq-local helm-dash-docsets '("UnderscoreJS" "React"))))
+(add-hook 'emacs-lisp-mode-hook '(lambda ()
+                                   (setq-local helm-dash-docsets '("Emacs Lisp"))
+                                   (setq helm-current-buffer (current-buffer))))
+
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (setq-local helm-dash-docsets '("Redis" "Ruby on Rails" "Ruby"))
+                             (setq helm-current-buffer (current-buffer))))
+
+(add-hook 'clojure-mode-hook '(lambda ()
+                                (setq-local helm-dash-docsets '("Clojure"))
+                                (setq helm-current-buffer (current-buffer))))
+
+(add-hook 'js2-mode-hook '(lambda ()
+                            (setq-local helm-dash-docsets '("UnderscoreJS" "React" "Jasmine"))
+                            (setq helm-current-buffer (current-buffer))))
+
+(add-hook 'js-mode-hook '(lambda ()
+                           (setq-local helm-dash-docsets '("UnderscoreJS" "React" "Jasmine"))
+                           (setq helm-current-buffer (current-buffer))))
+
+(add-hook 'web-mode-hook '(lambda ()
+                            (setq-local helm-dash-docsets '("UnderscoreJS" "React"))
+                            (setq helm-current-buffer (current-buffer))))
 
 (add-hook 'projectile-rails-mode-hook '(lambda()
 																				 (setq-local helm-dash-docsets '("Redis" "Ruby on Rails" "Ruby" "jQuery"))))
@@ -12,3 +30,5 @@
 (evil-define-key 'normal global-map (kbd "M-SPC") 'helm-dash-at-point)
 
 (setq helm-dash-browser-func 'eww-browse-url)
+
+(print "ran1!!!!11---11")
