0c6be7e252dc7c1a6e79bad4c4b872dedd341183
asok <adam.sokolnicki@gmail.com>
2015-07-18 10:01:29 +0200

    Add endless/visit-pull-request-url

--- inits/init-magit.el
+++ inits/init-magit.el
@@ -45,25 +45,21 @@
                                               (s-replace ":" "/" url))))
      url)))
 
-(defun asok/github-new-pr (arg)
-  (interactive "P")
-  (unless arg
-    (setq arg '(0)))
-  (let* ((url (magit-get "remote" "origin" "url"))
-         (user (if (string-match "github.com:\\(.+\\)/" url)
-                   (match-string 1 url))))
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/compare/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-current-remote)
+                       "url"))
+           (magit-get-current-branch))))
 
-    (browse-url
-     (concat
-      (if (s-contains? "@" url)
-          (replace-regexp-in-string "\\(.+\\)@" "https://"
-                                    (s-replace ".git" ""
-                                               (s-replace ":" "/" url)))
-        )
-      "/compare/master..."
-      user ":" (magit-get-current-branch)
-      )
-     url)))
+(eval-after-load 'magit
+  '(define-key magit-mode-map "V"
+     #'endless/visit-pull-request-url))
 
 (fset 'asok/magit-pull-upstream
       (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([70 21 70 117 112 115 116 114 101 97 109 return 134217848 107 109 97 99 99 114 backspace backspace 114 111 45 101 110 100 45 14 14] 0 "%d")) arg)))
