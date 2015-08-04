ff206b79e4d37da6720a92bb40a55d91c2c096ca
asok <asok@asokhost>
2015-08-04 13:02:16 +0200

    Start Custom-mode in evil-motion-state

--- inits/init-evil.el
+++ inits/init-evil.el
@@ -76,6 +76,7 @@
                        messages-buffer-mode
                        flycheck-error-list-mode
                        ag-mode
+                       Custom-mode
                        eww-mode)
          do (add-to-list 'evil-motion-state-modes mode))
 
