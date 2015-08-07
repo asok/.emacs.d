f23cbd39c74267d8e103e52a0dcf58188c3c6f15
asok <asok@asokhost>
2015-08-07 13:53:12 +0200

    Remove evil keybindings from magit's modes

--- inits/init-magit.el
+++ inits/init-magit.el
@@ -1,31 +1,5 @@
 (require 'magit)
 
-(evil-add-hjkl-bindings magit-branch-manager-mode-map 'spc
-  (kbd "C-f") 'evil-scroll-page-down
-  (kbd "C-b") 'evil-scroll-page-up)
-
-(evil-add-hjkl-bindings magit-status-mode-map 'spc
-  (kbd "C-f") 'evil-scroll-page-down
-  (kbd "C-b") 'evil-scroll-page-up
-  (kbd "K") 'magit-discard-item
-  (kbd "h") 'magit-toggle-diff-refine-hunk
-  (kbd "l") 'magit-key-mode-popup-logging
-  (kbd "{") 'magit-goto-previous-sibling-section
-  (kbd "}") 'magit-goto-next-sibling-section)
-
-(evil-add-hjkl-bindings magit-commit-mode-map 'spc
-  (kbd "{") 'magit-goto-previous-section
-  (kbd "}") 'magit-goto-next-section
-  )
-
-(evil-add-hjkl-bindings magit-log-mode-map 'spc
-  (kbd "C-f") 'evil-scroll-page-down
-  (kbd "C-b") 'evil-scroll-page-up
-  )
-
-(evil-add-hjkl-bindings magit-branch-manager-mode-map 'spc
-  (kbd "K") 'magit-discard-item)
-
 (require 'magit-blame)
 
 (defun asok/visit-github (arg)
@@ -63,3 +37,5 @@
 
 (fset 'asok/magit-pull-upstream
       (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([70 21 70 117 112 115 116 114 101 97 109 return 134217848 107 109 97 99 99 114 backspace backspace 114 111 45 101 110 100 45 14 14] 0 "%d")) arg)))
+
+(setq magit-push-always-verify nil)
